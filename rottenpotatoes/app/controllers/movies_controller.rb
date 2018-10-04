c3f8f621462ab1a56050ec8bab883b644d9be273
Edward Mondragon <edward.mondragon@tamu.edu>
2018-10-04 21:54:37 +0000

    Three Scenarios can now be done

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :director, :rating, :description, :release_date)
   end
 
   def show
@@ -30,7 +30,19 @@ class MoviesController < ApplicationController
       session[:ratings] = @selected_ratings
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
+    
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+  end
+  
+  def find_same_director
+    if params[:director] != nil
+      session[:director] = params[:director]
+      @movies = Movie.where(director: params[:director])
+    else
+      @movie = Movie.find params[:id]
+      flash[:notice] = "#{@movie.title} has no director!"
+      redirect_to movie_path(@movie)
+    end
   end
 
   def new
--- rottenpotatoes/db/migrate/20111119180638_create_movies.rb
+++ rottenpotatoes/db/migrate/20111119180638_create_movies.rb
@@ -2,6 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def up
     create_table :movies do |t|
       t.string :title
+      t.director :director
       t.string :rating
       t.text :description
       t.datetime :release_date
